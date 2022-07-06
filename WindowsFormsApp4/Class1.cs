50abf600c4385b0eaf47a6b2beaa6e5e8ccc26f6
user <user@LAPTOP-9SV4VQFP>
2022-07-06 16:49:48 +0800

    construtor&public/private

---
+++ WindowsFormsApp4/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    internal class Class1
+    {
+        public int PublicState;
+        private int privateState;
+        public Class1(int state)
+        {
+            PublicState = state;
+            if(PublicState == 0)
+            {
+                Console.WriteLine($"state={PublicState}");
+            }
+            else if (PublicState == 1)
+            {
+                Console.WriteLine($"state={PublicState}");
+            }
+            else if (PublicState == 2)
+            {
+                Console.WriteLine($"state={PublicState}");
+            }
+        }
+        public Class1()
+        {
+        }
+        public void PrintState(int state)
+        {
+            privateState = state;
+            if (privateState == 0)
+            {
+                Console.WriteLine($"state={privateState}");
+            }
+            else if (privateState == 1)
+            {
+                Console.WriteLine($"state={privateState}");
+            }
+            else if (privateState == 2)
+            {
+                Console.WriteLine($"state={privateState}");
+            }
+        }
+        public void ChangePrivateState(int state)
+        {
+            privateState=state;
+            Console.WriteLine($"state={state}");
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Class1 class1 = new Class1();
+            class1.PublicState = 2;
+            //class1.privateState = 2;
+            class1.ChangePrivateState(3);
+        }
+    }
+}
